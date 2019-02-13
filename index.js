6de2da9161f396c83d8fb5b1f7827d02ea857508
EssKayz <esskay@protonmail.com>
2019-02-13 13:00:40 +0200

    Ex 4.3

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require('./app.js')
 const conf = require('./utils/config')
 
-const PORT = conf.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(conf.PORT, () => {
+  console.log(`Server running on port ${conf.PORT}`)
 })
