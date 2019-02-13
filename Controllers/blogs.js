51c8dde0d285e2bc316d8cc9baac5606e1a0c8f3
EssKayz <esskay@protonmail.com>
2019-02-13 15:45:51 +0200

    Ex 4.10, 4.13

--- Controllers/blogs.js
+++ Controllers/blogs.js
@@ -1,26 +1,46 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const all = await Blog.find({})
+    response.json(all)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
     const name = body.title
     console.log(body)
     console.log(name)
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    try {
+        const saved = await blog.save()
+        response.status(201).json(saved)
+    } catch (ex) {
+        console.log(ex)
+    }
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+    try {
+        const blog = await Blog.findById(request.params.id)
+        if (blog) {
+            response.json(blog.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
 })
 
 module.exports = blogsRouter
--- tests/httptest.test.js
+++ tests/httptest.test.js
@@ -1,28 +1,73 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+const initialBlogs = [
+    {
+        likes: 1,
+        author: "DEROPER",
+        title: "mgsklasdf",
+        url: "4523463gsdfgds"
+    },
+    {
+        title: "gfdhfghfg",
+        author: "jkdfdghfghdfg",
+        url: "673573gfdhgfhdf",
+        likes: 2
+    },
+    {
+        title: "wertsfdgdfsg",
+        author: "5tsrtdhdsfgh",
+        url: "5463sdfgfghdf",
+        likes: 2
+    }
+]
+
+beforeEach(async () => {
+    await Blog.remove({})
+    for (i = 0; i < initialBlogs.length; i++) {
+        let blogObject = new Blog(initialBlogs[i])
+        await blogObject.save()
+    }
 })
 
-test('there are three blogs', async () => {
-    const response = await api.get('/api/blogs')
-  
-    expect(response.body.length).toBe(3)
-  })
+describe('blog api works', () => {
 
-  test('id field is correctly named', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
-  })
-  
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-afterAll(() => {
-  mongoose.connection.close()
+    test('there are three blogs', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body.length).toBe(3)
+    })
+
+    test('id field is correctly named', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
+
+    test('posting new blog works', async () => {
+        const newBlog = new Blog({
+            title: "TestAdd",
+            author: "AddTest",
+            url: "TestingOfAdds",
+            likes: 9001
+        })
+
+        await api.post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+    })
+
+    afterAll(() => {
+        mongoose.connection.close()
+    })
 })
