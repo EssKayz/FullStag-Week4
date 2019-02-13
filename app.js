ed99651d9ce4211a37a44f65fa8a68aa771223ca
EssKayz <esskay@protonmail.com>
2019-02-13 13:09:29 +0200

    Refactor to match instructions unclear

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogsRouter = require('./Controllers/blogs')
+const conf = require('./utils/config')
+const mongoose = require('mongoose')
+
+const url = conf.MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+    .then(result => { console.log('connected to MongoDB') }).catch((error) => { console.log('error connection to MongoDB:', error.message) })
+
 
 app.use(cors())
 app.use(bodyParser.json())
--- models/blog.js
+++ models/blog.js
@@ -1,11 +1,4 @@
-const conf = require('../utils/config')
 const mongoose = require('mongoose')
-
-const url = conf.MONGODB_URI
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true })
-    .then(result => { console.log('connected to MongoDB') }).catch((error) => { console.log('error connection to MongoDB:', error.message) })
 
 const blogSchema = new mongoose.Schema({
     title: String,
